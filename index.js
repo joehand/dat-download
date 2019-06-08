e1294bcd5f3270658b1ee60516b486368a3d7aad
Joe Hand <joe@hand.email>
2019-06-07 21:44:38 -0700

    add promises

--- index.js
+++ index.js
@@ -1,69 +1,88 @@
-var assert = require('assert')
-var fs = require('fs')
-var path = require('path')
-var Dat = require('dat-node')
-var mirror = require('mirror-folder')
-var pump = require('pump')
-var ram = require('random-access-memory')
-var mkdirp = require('mkdirp')
-var debug = require('debug')('dat-download')
+const assert = require('assert')
+const fs = require('fs')
+const path = require('path')
+const Dat = require('dat-node')
+const mirror = require('mirror-folder')
+const pump = require('pump')
+const ram = require('random-access-memory')
+const mkdirp = require('mkdirp')
+const debug = require('debug')('dat-download')
 
-module.exports = function (datPath, downloadDest, cb) {
-  assert.equal(typeof datPath, 'string', 'dat-download: string path required')
-  if (typeof downloadDest === 'function') {
-    cb = downloadDest
-    downloadDest = process.cwd()
-  }
-  assert.equal(typeof cb, 'function', 'dat-download: callback required')
+module.exports = (datPath, downloadDest = process.cwd(), { overwrite = true } = {}) => {
+  assert.strictEqual(typeof datPath, 'string', 'dat-download: string path required')
 
   if (datPath.indexOf('//') > -1) datPath = datPath.split('//')[1]
-  var key = 'dat://' + datPath.split('/')[0]
-  var entryPath = '/' + datPath.split('/').slice(1).join('/')
+  const key = 'dat://' + datPath.split('/')[0]
+  const entryPath = '/' + datPath.split('/').slice(1).join('/')
   if (entryPath === '/') downloadDest = path.join(downloadDest, datPath.split('/')[0])
   debug('downloadDir', downloadDest)
   debug('dat key', key)
   debug('dat path', entryPath)
 
-  Dat(ram, {key: key, sparse: true}, function (err, dat) {
-    if (err) return cb(err)
-    var archive = dat.archive
-    dat.joinNetwork()
-    archive.metadata.update(function () {
-      download(entryPath, function (err) {
-        if (err) return cb(err)
-        dat.close(cb)
-      })
+  return new Promise(async (resolve, reject) => {
+    const dat = await Dat(ram, { key: key, sparse: true })
+    const net = dat.joinNetwork()
+    net.once('connection', () => {
+      debug('Connected')
     })
 
-    function download (entryPath, cb) {
-      archive.stat(entryPath, function (err, stat) {
-        if (err) return cb(err)
-        if (stat.isDirectory()) downloadDir(entryPath, cb)
-        if (stat.isFile()) downloadFile(entryPath, cb)
+    dat.archive.metadata.update(async () => {
+      try {
+        await download(entryPath)
+        await dat.close()
+        resolve()
+      } catch (e) {
+        await dat.close()
+        reject(e)
+      }
+    })
+
+    function download (entryPath) {
+      return new Promise((resolve, reject) => {
+        dat.archive.stat(entryPath, async (err, stat) => {
+          if (err) return reject(err)
+          if (stat.isDirectory()) await downloadDir(entryPath)
+          else if (stat.isFile()) await downloadFile(entryPath)
+          resolve()
+        })
       })
     }
 
-    function downloadDir (dirname, cb) {
+    function downloadDir (dirname) {
       debug('downloading dir', dirname)
-      var dest = path.join(downloadDest, dirname)
-      fs.stat(dest, function (_, stat) {
-        // throw if dest exists
-        if (stat && stat.isDirectory()) return cb(new Error('Destination path exists:' + dest))
-        mkdirp(dest, function (err) {
-          if (err) return cb(err)
-          mirror({fs: archive, name: dirname}, dest, cb)
+      return new Promise((resolve, reject) => {
+        var dest = path.join(downloadDest, dirname)
+        fs.stat(dest, function (_, stat) {
+          // throw if dest exists
+          if (overwrite === false) if (stat && stat.isDirectory()) return reject(new Error('Destination path exists:' + dest))
+          mkdirp(dest, function (err) {
+            if (err) return reject(err)
+            debug('starting download')
+            const prog = mirror({ fs: dat.archive, name: dirname, keepExisting: true }, dest, (err) => {
+              if (err) return reject(err)
+              resolve()
+            })
+            prog.on('put', src => {
+              debug('Download', src.name)
+            })
+          })
         })
       })
     }
 
     function downloadFile (file, cb) {
-      mkdirp(downloadDest, function (err) {
-        if (err) return cb(err)
-        var dest = path.join(downloadDest, file)
-        debug('downloading file', file, 'to', dest)
-        var rs = archive.createReadStream(file)
-        var ws = fs.createWriteStream(path.join(downloadDest, file))
-        pump(rs, ws, cb)
+      return new Promise((resolve, reject) => {
+        mkdirp(downloadDest, (err) => {
+          if (err) return reject(err)
+          var dest = path.join(downloadDest, file)
+          debug('downloading file', file, 'to', dest)
+          var rs = dat.archive.createReadStream(file)
+          var ws = fs.createWriteStream(path.join(downloadDest, file))
+          pump(rs, ws, (err) => {
+            if (err) return reject(err)
+            resolve()
+          })
+        })
       })
     }
   })
